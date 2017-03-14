a328be87928a1b5ec7782339eb9831a12decf7f9
Michael H. Oshita <ijinpublic+github@gmail.com>
2017-03-14 11:31:00 +0900

    fix deprecated messeages

--- attributes/default.rb
+++ attributes/default.rb
@@ -58,11 +58,11 @@ default['rvm']['gpg_key']       = 'D39DC0E3'
 
 case platform
 when "redhat","centos","fedora","scientific","amazon"
-  node.set['rvm']['install_pkgs']   = %w{sed grep tar gzip bzip2 bash curl git}
+  default['rvm']['install_pkgs']   = %w{sed grep tar gzip bzip2 bash curl git}
 when "debian","ubuntu","suse"
-  node.set['rvm']['install_pkgs']   = %w{sed grep tar gzip bzip2 bash curl git-core}
+  default['rvm']['install_pkgs']   = %w{sed grep tar gzip bzip2 bash curl git-core}
 when "gentoo"
-  node.set['rvm']['install_pkgs']   = %w{git}
+  default['rvm']['install_pkgs']   = %w{git}
 when "mac_os_x", "mac_os_x_server"
-  node.set['rvm']['install_pkgs']   = %w{git}
+  default['rvm']['install_pkgs']   = %w{git}
 end
